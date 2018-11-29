5100850028bc18afd6c7b5f6bd91685300dae91e
bakmenson <bakmenson@gmail.com>
2018-11-29 23:11:52 +0300

    add main

---
+++ main.py
@@ -0,0 +1,105 @@
+from random import choice
+
+
+class DeckOfCards:
+    def __init__(self, card, symbol):
+        self._card = card
+        self._symbol = symbol
+        self._cd_name = choice([_ for _ in self._card.items()])
+        self._cd_sbl = choice([_ for _ in self._symbol.values()])
+
+    def get_card(self):
+        return [chr(9484) + (chr(9472) * 9) + chr(9488),
+                chr(9474) + ('\033[0;30;47m' + '{:<2}'.format(self._cd_name[0])
+                             + '\033[0m')
+                + (('\033[0;30;47m' + ' ' + '\033[0m') * 7) + chr(9474),
+
+                chr(9474) + (('\033[0;30;47m' + ' ' + '\033[0m') * 9)
+                + chr(9474),
+
+                chr(9474) + (('\033[0;30;47m' + ' ' + '\033[0m') * 9)
+                + chr(9474),
+
+                chr(9474) + (('\033[0;30;47m' + ' ' + '\033[0m') * 4)
+                + f"{self._cd_sbl}" + (
+                        ('\033[0;30;47m' + ' ' + '\033[0m') * 4) + chr(9474),
+                chr(9474) + (('\033[0;30;47m' + ' ' + '\033[0m') * 9)
+                + chr(9474),
+
+                chr(9474) + (('\033[0;30;47m' + ' ' + '\033[0m') * 9)
+                + chr(9474),
+
+                chr(9474) + (('\033[0;30;47m' + ' ' + '\033[0m') * 7)
+                + ('\033[0;30;47m' + '{:>2}'.format(
+                    self._cd_name[0]) + '\033[0m') + chr(9474),
+
+                chr(9492) + (chr(9472) * 9) + chr(9496)]
+    
+    def get_value(self):
+        return self._cd_name[1]
+
+
+class Player:
+    def __init__(self, card_list):
+        self.cds = card_list
+
+        if len(self.cds) >= 2:
+            self.cdl = [[_ for _ in self.cds[0].get_card()],
+                    [_ for _ in self.cds[1].get_card()]]
+
+            self.cdv = [self.cds[0].get_value(), self.cds[1].get_value()]
+        else:
+            self.cdl = [[_ for _ in self.cds[0].get_card()]]
+            self.cdv = [self.cds[0].get_value()]
+
+    def get_pl_cards(self):
+        return self.cdl
+
+    def get_cards_value(self):
+        return self.cdv
+
+
+def game(plr):
+    end_score = 21 if len(plr.get_pl_cards()) >= 2 else 17
+
+    while True:
+        plz = zip(*plr.get_pl_cards())
+
+        for x in plz:
+            print(*x)
+        
+        vpl = sum(plr.get_cards_value())
+        print(f'У вас {vpl} очков.')
+
+        if vpl == end_score:
+            print('you won!')
+            break
+        elif vpl > end_score:
+            print('перебор')
+            break
+        else:
+            plr.cds.append(DeckOfCards(cards, card_symbol))
+            plr.cdl.append([_ for _ in plr.cds[len(plr.cds) - 1].get_card()])
+            plr.cdv.append(plr.cds[len(plr.cds) - 1].get_value())
+        
+        # input()
+
+
+if __name__ == '__main__':
+    card_symbol = {
+        'spades': '\033[0;30;47m' + chr(9824) + '\033[0m',
+        'diamonds': '\033[0;31;47m' + chr(9830) + '\033[0m',
+        'hearts': '\033[0;31;47m' + chr(9829) + '\033[0m',
+        'clubs': '\033[0;30;47m' + chr(9827) + '\033[0m',
+    }
+
+    cards = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+             '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11}
+
+    player = Player([DeckOfCards(cards, card_symbol),
+                 DeckOfCards(cards, card_symbol)])
+
+    dealer = Player([DeckOfCards(cards, card_symbol)])
+
+    # game(player)
+    game(dealer)
