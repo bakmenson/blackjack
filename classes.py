f7ba975bf52bf9e4db8785d3b8b2049b44d314f9
bakmenson <bakmenson@gmail.com>
2019-09-18 18:50:30 +0300

    DeckOfCards: delete attribute card, added card_name, card_value

--- classes.py
+++ classes.py
@@ -3,14 +3,17 @@ from typing import Tuple, List
 
 
 class DeckOfCards:
+    card_name: str
+    card_value: int
+
     def __init__(self, cards: Tuple, symbols: Tuple) -> None:
-        self.card: Tuple[str, int] = choice([_ for _ in cards])
         self.symbol: str = choice([_ for _ in symbols])
+        self.card_name, self.card_value = choice([_ for _ in cards])
 
     def get_card(self) -> Tuple[str, ...]:
         return (
             f"{chr(9484)}{chr(9472) * 9}{chr(9488)}",
-            f"{chr(9474)}\033[0;30;47m{self.card[0]:<2}"
+            f"{chr(9474)}\033[0;30;47m{self.card_name:<2}"
             + f"\033[0m\033[0;30;47m \033[0m" * 7 + f"{chr(9474)}",
             f"{chr(9474)}" + f"\033[0;30;47m \033[0m" * 9 + f"{chr(9474)}",
             f"{chr(9474)}" + f"\033[0;30;47m \033[0m" * 9 + f"{chr(9474)}",
@@ -19,12 +22,12 @@ class DeckOfCards:
             f"{chr(9474)}" + f"\033[0;30;47m \033[0m" * 9 + f"{chr(9474)}",
             f"{chr(9474)}" + f"\033[0;30;47m \033[0m" * 9 + f"{chr(9474)}",
             f"{chr(9474)}" + f"\033[0;30;47m \033[0m" * 7
-            + f"\033[0;30;47m{self.card[0]:>2}\033[0m{chr(9474)}",
+            + f"\033[0;30;47m{self.card_name:>2}\033[0m{chr(9474)}",
             f"{chr(9492)}{chr(9472) * 9}{chr(9496)}"
         )
 
     def get_card_value(self) -> int:
-        return self.card[1]
+        return self.card_value
 
 
 class Player:
