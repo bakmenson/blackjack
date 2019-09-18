0306587fc175d079dcb6aaef3172de554e226902
bakmenson <bakmenson@gmail.com>
2019-09-18 23:46:51 +0300

    end_sep => end_sep_count

--- functions.py
+++ functions.py
@@ -3,18 +3,18 @@ from os import get_terminal_size
 
 
 def title(name: str) -> None:
-    end_sep: int = 0
+    end_sep_count: int = 0
     term_width: int = get_terminal_size()[0]
     len_title: int = ((int(term_width / 2) - int((len(name) / 2) + 1))
                       + (int(term_width / 2) - int((len(name) / 2) + 1))
                       + len(name)) + 2
 
     if len_title > term_width:
-        end_sep = 2
+        end_sep_count = 2
     if len_title == term_width:
-        end_sep = 1
+        end_sep_count = 1
     print('-' * (int(term_width / 2) - int((len(name) / 2) + 1)), name,
-          '-' * (int(term_width / 2) - int((len(name) / 2) + end_sep)))
+          '-' * (int(term_width / 2) - int((len(name) / 2) + end_sep_count)))
 
 
 def print_player_cards(player: Player) -> None:
