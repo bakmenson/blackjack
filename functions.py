f313bf2c68379eed6b9f47363fb784f281c74d99
bakmenson <bakmenson@gmail.com>
2019-09-18 23:05:17 +0300

    added function title()

--- functions.py
+++ functions.py
@@ -1,4 +1,21 @@
-import os
+from __future__ import annotations
+from os import get_terminal_size
+
+
+def title(name: str) -> None:
+    end_sep: int
+    term_width: int = get_terminal_size()[0]
+    len_title: int = ((int(term_width / 2) - int((len(name) / 2) + 1))
+                      + (int(term_width / 2) - int((len(name) / 2) + 1))
+                      + len(name)) + 2
+
+    end_sep = 0
+    if len_title > term_width:
+        end_sep = 2
+    if len_title == term_width:
+        end_sep = 1
+    print('-' * (int(term_width / 2) - int((len(name) / 2) + 1)), name,
+          '-' * (int(term_width / 2) - int((len(name) / 2) + end_sep)))
 
 
 def print_cards(player_cards):
