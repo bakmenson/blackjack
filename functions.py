4f56db77bed3b771ac58d55591d45eb02901aad0
bakmenson <bakmenson@gmail.com>
2019-09-19 10:44:03 +0300

    changed if, else condition

--- functions.py
+++ functions.py
@@ -3,16 +3,14 @@ from os import get_terminal_size
 
 
 def title(name: str) -> None:
-    end_sep_count: int = 0
     term_width: int = get_terminal_size()[0]
     len_title: int = ((int(term_width / 2) - int((len(name) / 2) + 1))
                       + (int(term_width / 2) - int((len(name) / 2) + 1))
                       + len(name)) + 2
 
-    if len_title > term_width:
-        end_sep_count = 2
-    if len_title == term_width:
-        end_sep_count = 1
+    end_sep_count: int = 2 if len_title > term_width else 1 \
+        if len_title == term_width else 0
+
     print('-' * (int(term_width / 2) - int((len(name) / 2) + 1)), name,
           '-' * (int(term_width / 2) - int((len(name) / 2) + end_sep_count)))
 
