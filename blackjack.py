c2463a0fb8d3ae6f2ef8253d9a08b7307746d259
bakmenson <bakmenson@gmail.com>
2019-09-19 11:17:04 +0300

    rename symbosl => card_suit s

--- blackjack.py
+++ blackjack.py
@@ -4,7 +4,7 @@ from random import choice
 
 game: bool = True
 
-symbols = (('\033[0;30;47m' + chr(9824) + '\033[0m'),
+card_suits = (('\033[0;30;47m' + chr(9824) + '\033[0m'),
            ('\033[0;31;47m' + chr(9830) + '\033[0m'),
            ('\033[0;31;47m' + chr(9829) + '\033[0m'),
            ('\033[0;30;47m' + chr(9827) + '\033[0m'))
@@ -12,8 +12,8 @@ symbols = (('\033[0;30;47m' + chr(9824) + '\033[0m'),
 cards = (('2', 2), ('3', 3), ('4', 4), ('5', 5), ('6', 6), ('7', 7), ('8', 8),
          ('9', 9), ('10', 10), ('J', 10), ('Q', 10), ('K', 10), ('A', 11))
 
-pl = Player([DeckOfCards(cards, symbols), DeckOfCards(cards, symbols)])
-dl = Player([DeckOfCards(cards, symbols)])
+pl = Player([DeckOfCards(cards, card_suits), DeckOfCards(cards, card_suits)])
+dl = Player([DeckOfCards(cards, card_suits)])
 
 print()
 title('Dialler\'s Cards')
