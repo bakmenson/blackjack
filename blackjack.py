9ec2d5132a1ad37a1e35d5ad7fe5b24abd465a5f
bakmenson <bakmenson@gmail.com>
2019-09-19 18:23:12 +0300

    rename dialler => dealer

--- blackjack.py
+++ blackjack.py
@@ -11,8 +11,8 @@ card_suits = (('\033[0;30;47m' + chr(9824) + '\033[0m'),
 cards = (('2', 2), ('3', 3), ('4', 4), ('5', 5), ('6', 6), ('7', 7), ('8', 8),
          ('9', 9), ('10', 10), ('J', 10), ('Q', 10), ('K', 10), ('A', 11))
 
-player_title, dialler_title = 'Player\'s Cards', 'Dialler\'s Cards'
+player_title, dealer_title = 'Player\'s Cards', 'Dealer\'s Cards'
 
-dialler = Player([DeckOfCards(cards, card_suits)])
+dealer = Player([DeckOfCards(cards, card_suits)])
 player = Player([DeckOfCards(cards, card_suits),
                  DeckOfCards(cards, card_suits)])
