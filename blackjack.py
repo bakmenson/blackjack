27113bcc79f30a0898b4ee1806dd308e308a9495
bakmenson <bakmenson@gmail.com>
2019-09-20 20:09:46 +0300

    changed title stings

--- blackjack.py
+++ blackjack.py
@@ -15,7 +15,7 @@ card_suits = (('\033[0;30;47m' + chr(9824) + '\033[0m'),
 cards = (('2', 2), ('3', 3), ('4', 4), ('5', 5), ('6', 6), ('7', 7), ('8', 8),
          ('9', 9), ('10', 10), ('J', 10), ('Q', 10), ('K', 10), ('A', 11))
 
-player_title, dealer_title = 'Player\'s Cards', 'Dealer\'s Cards'
+player_title, dealer_title = 'Your Cards', 'Dealer Cards'
 
 dealer = Player([DeckOfCards(cards, card_suits)])
 player = Player([DeckOfCards(cards, card_suits),
