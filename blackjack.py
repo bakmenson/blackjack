1898a680d02a8600735fb0502cb4a48e861e9c4e
bakmenson <bakmenson@gmail.com>
2019-09-23 23:46:39 +0300

    var cards to face_cards

--- blackjack.py
+++ blackjack.py
@@ -12,29 +12,37 @@ card_suits = (('\033[0;30;47m' + chr(9824) + '\033[0m'),
               ('\033[0;31;47m' + chr(9829) + '\033[0m'),
               ('\033[0;30;47m' + chr(9827) + '\033[0m'))
 
-cards = (('2', 2), ('3', 3), ('4', 4), ('5', 5), ('6', 6), ('7', 7), ('8', 8),
+face_cards = (('2', 2), ('3', 3), ('4', 4), ('5', 5), ('6', 6), ('7', 7), ('8', 8),
          ('9', 9), ('10', 10), ('J', 10), ('Q', 10), ('K', 10), ('A', 11))
 
 player_title, dealer_title = 'Ваши карты', 'Карты дилера'
 
-dealer = Player([DeckOfCards(cards, card_suits)])
-player = Player([DeckOfCards(cards, card_suits),
-                 DeckOfCards(cards, card_suits)])
+dealer = Player([DeckOfCards(face_cards, card_suits)])
+player = Player([DeckOfCards(face_cards, card_suits),
+                 DeckOfCards(face_cards, card_suits)])
 
 separator()
 print_player_cards(player)
+player.add_card(DeckOfCards(face_cards, card_suits))
+input()
+print_player_cards(player)
+player.add_card(DeckOfCards(face_cards, card_suits))
+input()
+print_player_cards(player)
 
-money: int = input_money()
-print(money)
-
-while game:
-    available_chips: Tuple[int, ...] = tuple(c for c in chips if money >= c)
-
-    print('\nВыберите фишку и укажите номер фишки.\n')
-    bet = make_bet(available_chips)
-    print(bet)
-    money -= bet
-
-    print(money)
-
-    game = False if not is_game() else True
+# money: int = input_money()
+# print(money)
+#
+# # TODO checkout output game stuff in same line, decorator for title
+#
+# while game:
+#     available_chips: Tuple[int, ...] = tuple(c for c in chips if money >= c)
+#
+#     print('\nВыберите фишку и укажите номер фишки.\n')
+#     bet = make_bet(available_chips)
+#     print(bet)
+#     money -= bet
+#
+#     print(money)
+#
+#     game = False if not is_game() else True
