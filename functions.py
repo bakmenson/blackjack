1096d7a3a74fe5453b537e26057700386050926b
bakmenson <bakmenson@gmail.com>
2019-09-24 18:39:43 +0300

    edited end_sep_count in title

--- functions.py
+++ functions.py
@@ -13,8 +13,8 @@ def title(name: str) -> None:
                       + (int(term_width / 2) - int((len(name) / 2) + 1))
                       + len(name)) + 2
 
-    end_sep_count: int = 2 if len_title > term_width else 1 \
-        if len_title == term_width else 0
+    end_sep_count: int = 2 if (len_title > term_width) else 1 \
+        if (len_title == term_width) else 0
 
     print('-' * (int(term_width / 2) - int((len(name) / 2) + 1)), name,
           '-' * (int(term_width / 2) - int((len(name) / 2) + end_sep_count)))
