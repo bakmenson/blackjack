162275bf0063bccc6ae09a9894bf3ad5040e1fcb
bakmenson <bakmenson@gmail.com>
2019-09-27 22:38:56 +0300

    added while loop for bet

--- blackjack.py
+++ blackjack.py
@@ -1,10 +1,8 @@
 from classes import DeckOfCards, Player
 from functions import print_player_cards, title, make_bet, separator, \
-    input_money, is_game
+    input_money, is_continue
 from typing import Tuple
 
-game: bool = True
-bet: int = 0
 chips: Tuple[int, ...] = (1, 5, 25, 50, 100, 500, 1000)
 
 card_suits = (('\033[0;30;47m' + chr(9824) + '\033[0m'),
@@ -34,11 +32,23 @@ separator()
 #     print('\033[10A')
 # print('\033[10B')
 
+game: bool = True
+bet: Tuple[int, ...] = ()
+bets: Tuple[int, ...] = ()
 money: int = input_money()
 
 while game:
-    print('\nВыберите фишку и укажите номер фишки.\n')
-    bet = make_bet(chips, money)
-    money -= bet
+    is_bet: bool = True
 
-    game = False if not is_game() else True
+    while is_bet:
+        bet = make_bet(chips, money)
+        bets += (bet[0],)
+        money -= bets[-1]
+
+        is_bet = False if not money else False \
+            if not is_continue('Сделать еще ставку') else True
+
+    sum_bets: int = sum(bets)
+    print(bet[1])
+
+    game = False if not is_continue('Продолжить игру') else True
