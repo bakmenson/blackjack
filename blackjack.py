2dd3e1c7c0c65f14be3ea8369dfb26d220e39b07
bakmenson <bakmenson@gmail.com>
2019-10-01 18:38:31 +0300

    033 -> x1b, added var deck

--- blackjack.py
+++ blackjack.py
@@ -8,56 +8,63 @@ from os import get_terminal_size
 term_width: int = get_terminal_size()[0]
 chips: Tuple[int, ...] = (1, 5, 25, 50, 100, 500, 1000)
 
-card_suits = (('\033[0;30;47m' + chr(9824) + '\033[0m'),
-              ('\033[0;31;47m' + chr(9830) + '\033[0m'),
-              ('\033[0;31;47m' + chr(9829) + '\033[0m'),
-              ('\033[0;30;47m' + chr(9827) + '\033[0m'))
+card_suits = ('\x1b[0;30;47m' + chr(9824) + '\x1b[0m',
+              '\x1b[0;31;47m' + chr(9830) + '\x1b[0m',
+              '\x1b[0;31;47m' + chr(9829) + '\x1b[0m',
+              '\x1b[0;30;47m' + chr(9827) + '\x1b[0m')
 
 face_cards = (
     ('2', 2), ('3', 3), ('4', 4), ('5', 5), ('6', 6), ('7', 7), ('8', 8),
     ('9', 9), ('10', 10), ('J', 10), ('Q', 10), ('K', 10), ('A', 11)
 )
 
+cards = tuple(tuple(zip((i,) * 4, card_suits)) for i in face_cards)
+deck = tuple((*i[0], i[1]) for card in cards for i in card)
+
+for i in deck:
+    print(i)
+
 player_title, dealer_title = 'Ваши карты', 'Карты дилера'
 
-dealer = Player([Card(face_cards, card_suits)])
-player = Player([Card(face_cards, card_suits), Card(face_cards, card_suits)])
+dealer = Player((Card(face_cards, card_suits),))
+player = Player((Card(face_cards, card_suits), Card(face_cards, card_suits)))
 
-system('clear')
-
+# system('clear')
+#
+# separator(term_width)
 # q = 'y'
 # while q == 'y':
-#     print_player_cards(player)
+#     print_player_cards(player, term_width)
 #     player.add_card(Card(face_cards, card_suits))
 #     print('Input')
 #     q = input('some input >>> ')
-#     print('\033[10A')
-# print('\033[10B')
-
-separator(term_width)
-money: int = input_money()
-
-bet: int = 0
-bets: Tuple[int, ...] = ()
-
-while True:
-    while True:
-        system('clear')
-        separator(term_width)
-        print('Сделайте ставку (укажите номер фишки).')
-        bet = make_bet(chips, money, term_width)
-        bets += (bet,)
-        money -= bets[-1]
-
-        separator(term_width)
-        if money and is_continue('Сделать еще ставку'):
-            continue
-        break
-
-    sum_bets: int = sum(bets)
-
-    system('clear')
-    separator(term_width)
-    if money and is_continue('Продолжить игру'):
-        continue
-    break
+#     print('\x1b[10A')
+# print('\x1b[10B')
+#
+# separator(term_width)
+# money: int = input_money()
+#
+# bet: int = 0
+# bets: Tuple[int, ...] = ()
+#
+# while True:
+#     while True:
+#         system('clear')
+#         separator(term_width)
+#         print('Сделайте ставку (укажите номер фишки).')
+#         bet = make_bet(chips, money, term_width)
+#         bets += (bet,)
+#         money -= bets[-1]
+#
+#         separator(term_width)
+#         if money and is_continue('Сделать еще ставку'):
+#             continue
+#         break
+#
+#     sum_bets: int = sum(bets)
+#
+#     system('clear')
+#     separator(term_width)
+#     if money and is_continue('Продолжить игру'):
+#         continue
+#     break
--- classes.py
+++ classes.py
@@ -5,14 +5,14 @@ from typing import Tuple, List
 class Card:
     __slots__ = ('card_name', 'card_suit', 'card_value')
 
-    def __init__(self, face_cards: Tuple, card_suits: Tuple) -> None:
+    def __init__(self, face_cards: Tuple, card_suits: Tuple[str, ...]) -> None:
         self.card_suit: str = choice([_ for _ in card_suits])
         self.card_name: str = choice([_[0] for _ in face_cards])
         self.card_value: int = choice([_[1] for _ in face_cards])
 
     def get_card(self) -> Tuple[str, ...]:
         return (
-            f"{chr(9616)}\033[0;30;47m{self.card_name:<2}\033[0m"
+            f"{chr(9616)}\x1b[0;30;47m{self.card_name:<2}\x1b[0m"
             + f"{chr(9608)}" * 7 + f"{chr(9612)}",
             f"{chr(9616)}" + f"{chr(9608)}" * 9 + f"{chr(9612)}",
             f"{chr(9616)}" + f"{chr(9608)}" * 9 + f"{chr(9612)}",
@@ -21,7 +21,7 @@ class Card:
             f"{chr(9616)}" + f"{chr(9608)}" * 9 + f"{chr(9612)}",
             f"{chr(9616)}" + f"{chr(9608)}" * 9 + f"{chr(9612)}",
             f"{chr(9616)}" + f"{chr(9608)}" * 7
-            + f"\033[0;30;47m{self.card_name:>2}\033[0m{chr(9612)}",
+            + f"\x1b[0;30;47m{self.card_name:>2}\x1b[0m{chr(9612)}",
         )
 
     def get_card_value(self) -> int:
