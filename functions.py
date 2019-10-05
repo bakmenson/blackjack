48e80646021297700351ae8fcde03ad1100f34d1
bakmenson <bakmenson@gmail.com>
2019-10-05 17:13:20 +0300

    edited raise and except IndexError for make_bet

--- functions.py
+++ functions.py
@@ -66,14 +66,13 @@ def make_bet(chips: Tuple[int, ...], money: int, term_width: int) -> int:
         try:
             chip_idx = int(input(f"\n{'>>>':>{int(term_width / 3) + 2}} ")) - 1
             if chip_idx < 0 or chip_idx > len(available_chips) - 1:
-                raise IndexError
+                raise IndexError(f"{'Wrong command. Command not found.'}\n")
         except ValueError:
-            print(f"{'':^{int(term_width / 2) - 17}}"
+            print(f"{'':^{int(term_width / 2) - 16}}"
                   f"{'Wrong command. Select chip number.'}\n")
             continue
-        except IndexError:
-            print(f"{'':^{int(term_width / 2) - 16}}"
-                  f"{'Wrong command. Command not found.'}\n")
+        except IndexError as e:
+            print(f"{'':^{int(term_width / 2) - 17}}", e)
             continue
         break
 
