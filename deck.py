ecccc36847cf4f56eeba4b866a17d168139ec73e
bakmenson <bakmenson@gmail.com>
2019-10-10 14:32:37 +0300

    made deck private

--- deck.py
+++ deck.py
@@ -5,8 +5,8 @@ from dataclasses import dataclass, field
 
 @dataclass
 class Deck:
-    deck: Tuple = field(default_factory=tuple)
+    _deck: Tuple = field(default_factory=tuple)
 
     def get_card(self, num: int = 1) -> Tuple[Tuple[int, str, str], ...]:
         """Method return 1 (default) or num cards from deck of cards"""
-        return tuple(sample(self.deck, num))
+        return tuple(sample(self._deck, num))
