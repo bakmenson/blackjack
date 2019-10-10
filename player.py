11ed736b9b4a4a8f774292fd47eb7b0bbdca755a
bakmenson <bakmenson@gmail.com>
2019-10-10 14:29:42 +0300

    made Player attrs private

--- player.py
+++ player.py
@@ -4,24 +4,24 @@ from dataclasses import dataclass, field
 
 @dataclass
 class Player:
-    name: str = 'unknown player'
-    cards: Tuple[Tuple[int, str, str], ...] = field(default_factory=tuple)
+    _name: str = 'unknown player'
+    _cards: Tuple[Tuple[int, str, str], ...] = field(default_factory=tuple)
 
     def add_card(self, card: Tuple[Tuple[int, str, str], ...]) -> \
             Tuple[Tuple[int, str, str], ...]:
-        self.cards += (*card,)
-        return self.cards
+        self._cards += (*card,)
+        return self._cards
 
     def get_cards(self) -> Tuple[Tuple[int, str, str], ...]:
-        return self.cards
+        return self._cards
 
     @property
     def get_scores(self) -> int:
-        return sum((num[0] for num in self.cards))
+        return sum((num[0] for num in self._cards))
 
     def remove_cards(self) -> Tuple:
-        self.cards = tuple()
-        return self.cards
+        self._cards = tuple()
+        return self._cards
 
     def get_name(self) -> str:
-        return self.name
+        return self._name
