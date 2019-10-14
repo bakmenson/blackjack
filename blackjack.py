7f727fb6134907104759dd8992c3451c7161f7ac
bakmenson <bakmenson@gmail.com>
2019-10-14 15:59:24 +0300

    changed form var cards

--- blackjack.py
+++ blackjack.py
@@ -12,15 +12,14 @@ card_suits = ('\x1b[0;30;47m' + chr(9824) + '\x1b[0m',
               '\x1b[0;31;47m' + chr(9829) + '\x1b[0m',
               '\x1b[0;30;47m' + chr(9827) + '\x1b[0m')
 
-face_cards = (
-    (2, '2'), (3, '3'), (4, '4'), (5, '5'), (6, '6'), (7, 7), (8, '8'),
-    (9, '9'), (10, '10'), (10, 'J'), (10, 'Q'), (10, 'K'), (11, 'A')
-)
+card_faces = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
+card_values = [*[_ for _ in range(2, 10)], *[10 for _ in range(4)], 11]
 
 # get 4 face_cards for each card_suits
 cards: Tuple[Any, ...] = tuple(
-    (*i[0], i[1])
-    for card in (zip((_,) * 4, card_suits) for _ in face_cards)
+    (*i[0], i[1]) for card in (
+        zip((_,) * 4, card_suits) for _ in zip(card_values, card_faces)
+    )
     for i in card
 )
 
