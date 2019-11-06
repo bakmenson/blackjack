103bc5254b50773065858cf2aa995c9eb34bbc56
bakmenson <bakmenson@gmail.com>
2019-11-06 20:55:11 +0300

    add method set_cards, rename method add_card -> hit

--- player.py
+++ player.py
@@ -5,14 +5,23 @@ from dataclasses import dataclass, field
 @dataclass
 class Player:
     _name: str = 'unknown player'
-    _cards: List[Tuple[int, str, str]] = field(default_factory=list)
+    _cards: Union[
+        List[Tuple[int, str, str]],
+        List[List[Tuple[int, str, str]]]
+    ] = field(default_factory=list)
 
-    def add_card(
-            self,
-            card: Union[
-                List[Tuple[int, str, str]], List[List[Tuple[int, str, str]]]
-            ],
-            idx=None
+    def set_cards(self, cards: List[Tuple[int, str, str]]) -> None:
+        if len(cards) == 2:
+            self._cards = [cards]
+        else:
+            self._cards = cards
+
+    def hit(
+        self,
+        card: Union[
+            List[Tuple[int, str, str]], List[List[Tuple[int, str, str]]]
+        ],
+        idx=None
     ) -> None:
         """Method add card into player cards"""
         if idx is None:
@@ -20,12 +29,15 @@ class Player:
         if idx is not None:
             self._cards[idx].extend(card)
 
+    @property
     def get_cards(self) -> List[Tuple[int, str, str]]:
         """Method returns player cards"""
         return self._cards
 
-    def get_score(self, card_index: int = 0):
+    def get_score(self, card_index=None):
         """Method returns player scores"""
+        if card_index is None:
+            return sum(num[0] for num in self._cards)
         return sum(num[0] for num in self._cards[card_index])
 
     @property
