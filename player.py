1ca65c5a08ce27ed5e96ad9c690ca1f34f97bc2f
bakmenson <bakmenson@gmail.com>
2019-11-13 11:45:21 +0300

    added attr money for Player

--- player.py
+++ player.py
@@ -57,7 +57,18 @@ class BasePlayer:
         return self._name
 
 
+@dataclass
 class Player(BasePlayer):
+    _money: Union[int, float] = 0
+
+    @property
+    def money(self) -> Union[int, float]:
+        return self._money
+
+    @money.setter
+    def money(self, value: Union[int, float]) -> None:
+        self._money = value
+
     def split_cards(
             self,
             card_index: int,
